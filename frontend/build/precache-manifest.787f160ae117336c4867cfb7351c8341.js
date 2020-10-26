9e993b6333f9678b7d497fa8afd3a10412559cab
Jasper <jasperhaward@virginmedia.com>
2020-10-26 13:17:41 +0000

    Added build directory

---
+++ frontend/build/precache-manifest.787f160ae117336c4867cfb7351c8341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dbf788baafdceb8ec238925b311dabd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3a428d7ec2934336002",
+    "url": "/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "0aca8683c77e353fd55d",
+    "url": "/static/css/main.5be2bae3.chunk.css"
+  },
+  {
+    "revision": "c3a428d7ec2934336002",
+    "url": "/static/js/2.03d09b74.chunk.js"
+  },
+  {
+    "revision": "6707ff4babf89ebfd59223416c7ff80e",
+    "url": "/static/js/2.03d09b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aca8683c77e353fd55d",
+    "url": "/static/js/main.8342de52.chunk.js"
+  },
+  {
+    "revision": "d94f65e27950a80144b5",
+    "url": "/static/js/runtime-main.018239c2.js"
+  }
+]);
