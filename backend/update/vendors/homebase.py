6e1fc55ffbddc40c5827fa7a57d23578a741a09b
jasper <jasper@>
2020-12-07 13:00:08 +0000

    Edited order number regex

--- backend/update/vendors/homebase.py
+++ backend/update/vendors/homebase.py
@@ -1,16 +1,18 @@
 import re
 import dateutil.parser
 
+
 def format_homebase_order(order):
-    items = ['','','']
+    items = ['', '', '']
     for index, item in enumerate(order['line_items']):
         if index > 2:
-            items[2] = items[2] + ' & ' + item['sellable']['product_title'].upper()
+            items[2] = items[2] + ' & ' + \
+                item['sellable']['product_title'].upper()
         else:
             items[index] = item['sellable']['product_title'].upper()
 
-    po_num = re.findall(r"\d{10}", order['customer_note']['text'])
-    
+    po_num = re.findall(r"4\d{9}", order['customer_note']['text'])
+
     return [
         '',
         dateutil.parser.parse(order['created_at']).strftime(r"%d/%m/%Y"),
@@ -27,6 +29,5 @@ def format_homebase_order(order):
         items[2],
         order['deliver_to']['first_name'].upper(),
         order['deliver_to']['last_name'].upper(),
-        order['deliver_to']['zip'].upper(),        
+        order['deliver_to']['zip'].upper(),
     ]
-                
