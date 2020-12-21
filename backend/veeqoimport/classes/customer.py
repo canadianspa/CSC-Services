93c16ef09d0008d41154846868f5339f45e3b166
jasper <jasper@>
2020-12-21 10:29:04 +0000

    Updated format structure

--- backend/veeqoimport/classes/customer.py
+++ backend/veeqoimport/classes/customer.py
@@ -1,44 +1,47 @@
 class Customer:
     def __init__(
-        self, 
-        first_name, 
-        last_name, 
-        address1, 
-        address2 = None, 
-        city = None, 
-        state = None, 
-        zip_code = None, 
-        country = None, 
-        phone = None, 
-        email = None
+        self,
+        first_name,
+        last_name,
+        address1,
+        address2=None,
+        city=None,
+        state=None,
+        zip_code=None,
+        country=None,
+        phone=None,
+        email=None
     ):
         if last_name == "" and first_name == "":
-            raise ValueError(f"first_name: {first_name}, last_name: {last_name}")
+            raise ValueError(
+                f"first_name: {first_name}, last_name: {last_name}")
         if address1 == "":
             raise ValueError(f"address1: {address1}")
         if zip_code == "":
             raise ValueError(f"zip_code: {zip_code}")
         if phone == "":
             raise ValueError(f"phone: {phone}")
-            
+
         self.id = ""
-        self.first_name = format(first_name)
-        self.last_name = format(last_name)
+        self.first_name = self._format(first_name)
+        self.last_name = self._format(last_name)
         self.company = ""
-        self.address1 = format(address1)
-        self.address2 = format(address2)
-        self.city = format(city)
-        self.state = format(state)
+        self.address1 = self._format(address1)
+        self.address2 = self._format(address2)
+        self.city = self._format(city)
+        self.state = self._format(state)
         self.zip = zip_code
-        self.country = format(country)
+        self.country = self._format(country)
         self.phone = phone
-        self.email = format(email)
+        self.email = self._format(email)
 
-    
-    def format(self, string):
-        return string.title().replace(";", "") or ""
-
+    def _format(self, string):
+        if string is not None:
+            return string.title().replace(";", "") or ""
+        else:
+            return ""
 
     # For rich comparisons between 2 customer classes
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
