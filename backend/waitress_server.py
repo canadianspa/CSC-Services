b364a8b14b92c4568bb470625a3b025f02687792
Jasper <jasperhaward@virginmedia.com>
2021-02-04 13:20:20 +0000

    Added logging

--- backend/waitress_server.py
+++ backend/waitress_server.py
@@ -1,4 +1,5 @@
 from waitress import serve
 import app
+from paste.translogger import TransLogger
 
-serve(app.app, host='0.0.0.0', port=5000)
+serve(TransLogger(app.app), host='0.0.0.0', port=5000)
