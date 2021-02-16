2450513ee7add42f535a503cafd6b90c2f1163ec
jasper <jasper@>
2021-02-16 15:48:01 +0000

    Attempted fix of multiple additional notes

--- backend/notifications/main.py
+++ backend/notifications/main.py
@@ -1,5 +1,4 @@
 import atexit
-import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from datetime import datetime
@@ -7,26 +6,32 @@ from dateutil import parser
 
 from common.api.veeqo import get_cancelled_orders, create_order_note
 
+
 def cancellation_notification():
     # U23425 = ANIQA, U27768 = ROBBIE
-    message =  "[@U23425] [@U27768]: ORDER CANCELLED"
+    message = "[@U23425] [@U27768]: ORDER CANCELLED"
 
     orders = get_cancelled_orders()
 
     for order in orders:
-        if order["cancelled_at"]:
-            cancelled_at = parser.parse(order["cancelled_at"])
+        _id = order["id"]
+        cancelled_at = order["cancelled_at"]
+        if cancelled_at:
+            cancelled_at = parser.parse(cancelled_at)
             now = datetime.now()
 
             if cancelled_at.date() == now.date():
-                create_order_note(
-                    order["id"], 
-                    message,
-                )
+                create_order_note(_id, message)
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=cancellation_notification, trigger='cron', hour='23', minute='45')
+scheduler.add_job(
+    func=cancellation_notification,
+    trigger='cron',
+    hour='23',
+    minute='45',
+    second='00',
+)
 scheduler.start()
 
 # Shut down the scheduler when exiting the app
